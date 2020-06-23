374cc895e2e1e17da56064757b5e24bbd16c54ca
Sergio Alarcón Felipe <sergio.alarcon.felipe@gmail.com>
2020-06-24 00:40:41 +0200

    Update jsdz-core.js

--- src/jsdz-core.js
+++ src/jsdz-core.js
@@ -1,6 +1,20 @@
 ( function() {
 	let jsdz = function() {
+        this.customEvent = function( type ) {
+            if( type === undefined || typeof type !== 'string' ) {
+                return null;
+            }
+            
+            var options = {
+                bubbles: true,
+                cancelable: true,
+            };
 
+            return new CustomEvent(
+                type, 
+                options
+            );
+        }
 	};
 
 	window.jsdz = new jsdz();
