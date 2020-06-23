62486550f83d1b69ad8551fc087fe430c11b59ab
Sergio Alarcón Felipe <sergio.alarcon.felipe@gmail.com>
2020-06-23 18:00:41 +0200

    Update jsdz-onepage.js

--- src/jsdz-onepage/jsdz-onepage.js
+++ src/jsdz-onepage/jsdz-onepage.js
@@ -1,1 +1,29 @@
+document.addEventListener( 'DOMContentLoaded', function() {
+	window.jsdz.pageUpdate = function() {
+		document.querySelectorAll( 'div[ data-role = "page" ]' ).forEach( function( page ) {
+			if( page.show === undefined ) {
+				page.show = function() {
+					if( !this.classList.contains( 'active' ) ) {
+						document.querySelectorAll( 'div[ data-role = "page" ]' ).forEach( function( page ) {
+							page.classList.remove( 'active' );
+						} );
+						this.classList.add( 'active' );
+					};
+				};
+			}
+		} );
+	}
 
+	window.jsdz.pageLinkUpdate = function() {
+		function pageLinkClick() {
+			document.querySelector( this.getAttribute( 'data-target' ) ).show();
+		}
+		document.querySelectorAll( 'span[ data-role = "pageLink" ]' ).forEach( function( link ) {
+			link.removeEventListener( 'click', pageLinkClick );
+			link.addEventListener( 'click', pageLinkClick );
+		} );
+	}
+	
+	window.jsdz.pageLinkUpdate();
+	window.jsdz.pageUpdate();
+} );
