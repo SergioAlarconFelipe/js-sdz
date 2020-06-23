a32155c2b9c1f004e10456165c927d9f9d82f76f
Sergio Alarcón Felipe <sergio.alarcon.felipe@gmail.com>
2020-06-23 22:13:55 +0200

    Update jsdz-onepage.js

--- src/jsdz-onepage/jsdz-onepage.js
+++ src/jsdz-onepage/jsdz-onepage.js
@@ -9,45 +9,59 @@
 				page.showOnly = function() {
 					if( !this.classList.contains( 'visible' ) ) {
 						document.querySelectorAll( 'div[ data-role = "page" ]' ).forEach( function( page ) {
-							page.classList.remove( 'visible' );
+							if( page.classList.contains( 'visible' ) ) {
+								page.dispatchEvent( window.jsdz.customEvent( 'beforeHide' ) );
+								page.classList.remove( 'visible' );
+								page.dispatchEvent( window.jsdz.customEvent( 'afterHide' ) );
+							}
 						} );
+						this.dispatchEvent( window.jsdz.customEvent( 'beforeShow' ) );
 						this.classList.add( 'visible' );
+						this.dispatchEvent( window.jsdz.customEvent( 'afterShow' ) );
 					};
 				};
 			}
-			
+
 			if( page.show === undefined ) {
 				page.show = function() {
-					this.classList.add( 'visible' );
+					if( !page.classList.contains( 'visible' ) ) {
+						this.dispatchEvent( window.jsdz.customEvent( 'beforeShow' ) );
+						this.classList.add( 'visible' );
+						this.dispatchEvent( window.jsdz.customEvent( 'afterShow' ) );
+					}
 				};
 			}
-			
+
 			if( page.hide === undefined ) {
 				page.hide = function() {
 					if( !this.classList.contains( 'visible' ) ) {
-						this.classList.remove( 'visible' );
+						if( page.classList.contains( 'visible' ) ) {
+							this.dispatchEvent( window.jsdz.customEvent( 'beforeHide' ) );
+							this.classList.remove( 'visible' );
+							this.dispatchEvent( window.jsdz.customEvent( 'afterHide' ) );
+						}
 					};
 				};
 			}
 		} );
-	}
+	};
 
 	window.jsdz.pageLinkUpdate = function() {
 		function pageLinkClick() {
-		    var targets = document.querySelectorAll( this.getAttribute( 'data-target' ) ) || [];
+			var targets = document.querySelectorAll( this.getAttribute( 'data-target' ) ) || [];
 
-		    targets.forEach( function( target ) {
+			targets.forEach( function( target ) {
 				if( target.getAttribute( 'data-role' ) === 'page' ) {
 					target.showOnly();
 				}
-		    } );
+			} );
 		}
 
 		document.querySelectorAll( 'span[ data-role = "pageLink" ]' ).forEach( function( link ) {
-		    link.removeEventListener( 'click', pageLinkClick );
-		    link.addEventListener( 'click', pageLinkClick );
+			link.removeEventListener( 'click', pageLinkClick );
+			link.addEventListener( 'click', pageLinkClick );
 		} );
-	}
+	};
 
 	document.addEventListener( 'DOMContentLoaded', function() {
 		window.jsdz.pageLinkUpdate();
