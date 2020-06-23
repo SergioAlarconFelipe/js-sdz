23a29cf0f9904c952d6c3dcf132e16df68a62b0c
Sergio Alarcón Felipe <sergio.alarcon.felipe@gmail.com>
2020-06-23 18:09:57 +0200

    Update jsdz-swipe.js

--- src/jsdz-swipe/jsdz-swipe.js
+++ src/jsdz-swipe/jsdz-swipe.js
@@ -1,81 +1,85 @@
-document.addEventListener( 'DOMContentLoaded', function() {
-	( function( element ) {
-		element.addEventListener( 'touchstart', start, false );
-		element.addEventListener( 'touchmove', move, false ); 
-		element.addEventListener( 'touchend', end, false );
+( function( element ) {
+	if( window.jsdz === undefined ) {
+		throw new Error( '\njsdz no se ha defectado.\nPara masinformacion visita: https://github.com/SergioAlarconFelipe/jsdz' );
+	}
 
-		element.addEventListener( 'mousedown', start, false );
-		element.addEventListener( 'mousemove', move, false );
-		element.addEventListener( 'mouseup', end, false );
+	element.addEventListener( 'touchstart', start, false );
+	element.addEventListener( 'touchmove', move, false ); 
+	element.addEventListener( 'touchend', end, false );
 
-		var pxSensibility = 100;
-		var touching = false;
-		var direction = 'none';
-		var touchX = false;
-		var cE = null;
+	element.addEventListener( 'mousedown', start, false );
+	element.addEventListener( 'mousemove', move, false );
+	element.addEventListener( 'mouseup', end, false );
 
-		function start( event ) {
-			touching = true;
-			touchX = event.x;
-		}
-		function end( event ) {
-			touching = false;
-			direction = 'none';
-		}
-		function move( event ) {
-			if( touching ) {
-				if( direction === 'none' ) {
-					if( event.x > touchX ) {
-						direction = 'right';
-					}
-					else if( event.x < touchX ) {
-						direction = 'left';
-					}
+	var pxSensibility = 100;
+	var touching = false;
+	var direction = 'none';
+	var touchX = false;
+	var cE = null;
+
+	function start( event ) {
+		touching = true;
+		touchX = event.x;
+	}
+	function end( event ) {
+		touching = false;
+		direction = 'none';
+	}
+	function move( event ) {
+		if( touching ) {
+			if( direction === 'none' ) {
+				if( event.x > touchX ) {
+					direction = 'right';
 				}
-
-				if( direction === 'right' && event.x < touchX ) {
-					touching = false;
-					direction = 'none';
-				}
-				else if( direction === 'left' && event.x > touchX ) {
-					touching = false;
-					direction = 'none';
-				}
-
-				if( event.x >= touchX + pxSensibility ) {
-					touching = false;
-					direction = 'none';
-
-					cE = new CustomEvent(
-						'swiperight', 
-						{
-							bubbles: true,
-							cancelable: true
-						}
-					);
-					event.srcElement.dispatchEvent( cE );
-				}
-				else if( event.x <= touchX - pxSensibility ) {
-					touching = false;
-					direction = 'none';
-
-					cE = new CustomEvent(
-						'swipeleft', 
-						{
-							bubbles: true,
-							cancelable: true
-						}
-					);
-					event.srcElement.dispatchEvent( cE );
+				else if( event.x < touchX ) {
+					direction = 'left';
 				}
 			}
+
+			if( direction === 'right' && event.x < touchX ) {
+				touching = false;
+				direction = 'none';
+			}
+			else if( direction === 'left' && event.x > touchX ) {
+				touching = false;
+				direction = 'none';
+			}
+
+			if( event.x >= touchX + pxSensibility ) {
+				touching = false;
+				direction = 'none';
+
+				cE = new CustomEvent(
+					'swiperight', 
+					{
+						bubbles: true,
+						cancelable: true
+					}
+				);
+				event.srcElement.dispatchEvent( cE );
+			}
+			else if( event.x <= touchX - pxSensibility ) {
+				touching = false;
+				direction = 'none';
+
+				cE = new CustomEvent(
+					'swipeleft', 
+					{
+						bubbles: true,
+						cancelable: true
+					}
+				);
+				event.srcElement.dispatchEvent( cE );
+			}
 		}
-	} )( document );
-} );
+	}
+} )( document );
 
 // Example
 /*
-document.getElementById( 'panel-1' ).addEventListener( 'swiperight', function( event ) {
-	console.log( 1, event );
+document.addEventListener( 'DOMContentLoaded', function() {
+	document.getElementById( 'panel-1' ).addEventListener( 'swiperight', function( event ) {
+		console.log( 1, event );
+	} );
 } );
 */
