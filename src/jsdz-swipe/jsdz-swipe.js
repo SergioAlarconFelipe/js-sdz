fb305297588f573914fab2e20e712ea667a3fb15
Sergio Alarcón Felipe <sergio.alarcon.felipe@gmail.com>
2020-06-24 01:20:17 +0200

    Update jsdz-swipe.js

--- src/jsdz-swipe/jsdz-swipe.js
+++ src/jsdz-swipe/jsdz-swipe.js
@@ -6,11 +6,11 @@
 	element.addEventListener( 'touchstart', start, false );
 	element.addEventListener( 'touchmove', move, false ); 
 	element.addEventListener( 'touchend', end, false );
-
+    
 	element.addEventListener( 'mousedown', start, false );
 	element.addEventListener( 'mousemove', move, false );
 	element.addEventListener( 'mouseup', end, false );
-
+    
 	var pxSensibility = 100;
 	var touching = false;
 	var direction = 'none';
@@ -18,15 +18,27 @@
 	var cE = null;
 
 	function start( event ) {
+        if( event.changedTouches !== undefined ) {
+            event.x = event.changedTouches[ 0 ].clientX;
+        }
+
 		touching = true;
 		touchX = event.x;
 	}
 	function end( event ) {
+        if( event.changedTouches !== undefined ) {
+            event.x = event.changedTouches[ 0 ].clientX;
+        }
+
 		touching = false;
 		direction = 'none';
 	}
 	function move( event ) {
 		if( touching ) {
+            if( event.changedTouches !== undefined ) {
+                event.x = event.changedTouches[ 0 ].clientX;
+            }
+            
 			if( direction === 'none' ) {
 				if( event.x > touchX ) {
 					direction = 'right';
@@ -44,11 +56,11 @@
 				touching = false;
 				direction = 'none';
 			}
-
+            
 			if( event.x >= touchX + pxSensibility ) {
 				touching = false;
 				direction = 'none';
-
+    
 				cE = new CustomEvent(
 					'swiperight', 
 					{
