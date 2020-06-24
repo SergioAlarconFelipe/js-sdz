69e39dc8d9ab3f160134a5406a4ac77db491531e
Sergio Alarcón Felipe <sergio.alarcon.felipe@gmail.com>
2020-06-24 11:44:16 +0200

    Update jsdz-swipe.js

--- src/jsdz-swipe/jsdz-swipe.js
+++ src/jsdz-swipe/jsdz-swipe.js
@@ -6,11 +6,11 @@
 	element.addEventListener( 'touchstart', start, false );
 	element.addEventListener( 'touchmove', move, false ); 
 	element.addEventListener( 'touchend', end, false );
-    
+
 	element.addEventListener( 'mousedown', start, false );
 	element.addEventListener( 'mousemove', move, false );
 	element.addEventListener( 'mouseup', end, false );
-    
+
 	var pxSensibility = 100;
 	var touching = false;
 	var direction = 'none';
@@ -18,27 +18,27 @@
 	var cE = null;
 
 	function start( event ) {
-        if( event.changedTouches !== undefined ) {
-            event.x = event.changedTouches[ 0 ].clientX;
-        }
+		if( event.changedTouches !== undefined ) {
+		    event.x = event.changedTouches[ 0 ].clientX;
+		}
 
 		touching = true;
 		touchX = event.x;
 	}
 	function end( event ) {
-        if( event.changedTouches !== undefined ) {
-            event.x = event.changedTouches[ 0 ].clientX;
-        }
+		if( event.changedTouches !== undefined ) {
+		    event.x = event.changedTouches[ 0 ].clientX;
+		}
 
 		touching = false;
 		direction = 'none';
 	}
 	function move( event ) {
 		if( touching ) {
-            if( event.changedTouches !== undefined ) {
-                event.x = event.changedTouches[ 0 ].clientX;
-            }
-            
+		    if( event.changedTouches !== undefined ) {
+				event.x = event.changedTouches[ 0 ].clientX;
+		    }
+
 			if( direction === 'none' ) {
 				if( event.x > touchX ) {
 					direction = 'right';
@@ -56,11 +56,11 @@
 				touching = false;
 				direction = 'none';
 			}
-            
+
 			if( event.x >= touchX + pxSensibility ) {
 				touching = false;
 				direction = 'none';
-    
+
 				cE = new CustomEvent(
 					'swiperight', 
 					{
@@ -85,13 +85,13 @@
 			}
 		}
 	}
-} )( document );
+	} )( document );
 
-// Example
-/*
-document.addEventListener( 'DOMContentLoaded', function() {
+	// Example
+	/*
+	document.addEventListener( 'DOMContentLoaded', function() {
 	document.getElementById( 'panel-1' ).addEventListener( 'swiperight', function( event ) {
 		console.log( 1, event );
 	} );
-} );
-*/
+	} );
+	*/
