4f71ed3e22285886a8277c9ad78b0f1037ec9fd1
Sergio Alarcón Felipe <sergio.alarcon.felipe@gmail.com>
2020-06-24 14:04:07 +0200

    Update jsdz-hidemenu.js

--- src/jsdz-hidemenu/jsdz-hidemenu.js
+++ src/jsdz-hidemenu/jsdz-hidemenu.js
@@ -3,7 +3,7 @@
 		throw new Error( '\njsdz no se ha defectado.\nPara masinformacion visita: https://github.com/SergioAlarconFelipe/jsdz' );
 	}
 
-	window.jsdz.pageUpdate = function() {
+	window.jsdz.menuUpdate = function() {
 		document.querySelectorAll( 'div[ data-role = "menu" ]' ).forEach( function( menu ) {
 			if( menu.show === undefined ) {
 				menu.show = function() {
@@ -38,18 +38,19 @@
 				};
 			}
 		} );
-	}
+	};
+    
+    function menuLinkClick() {
+        var target = document.getElementById( this.getAttribute( 'data-target' ) ) || null;
+        if( target && target.getAttribute( 'data-role' ) === 'menu' ) {
+            target.show();
+        }
+    }
 
 	window.jsdz.menuLinkUpdate = function() {
-		function pageLinkClick() {
-			var target = document.getElementById( this.getAttribute( 'data-target' ) ) || null;
-			if( target && target.getAttribute( 'data-role' ) === 'menu' ) {
-				target.show();
-			}
-		}
 		document.querySelectorAll( 'span[ data-role = "menuLink" ]' ).forEach( function( link ) {
-			link.removeEventListener( 'click', pageLinkClick );
-			link.addEventListener( 'click', pageLinkClick );
+			link.removeEventListener( 'click', menuLinkClick );
+			link.addEventListener( 'click', menuLinkClick );
 		} );
 	};
 
@@ -58,10 +59,8 @@
 			var fondo = document.createElement("div");
 			fondo.setAttribute( 'data-role', 'menuBackground' );
 			fondo.setAttribute( 'data-link', '#' + menu.id );
-			// fondo.appendChild( document.createTextNode("Water") );
 
 			menu.parentElement.insertBefore( fondo, menu.parentElement.firstChild );
-			// menu.parentElement.appendChild( fondo );
 
 			document.querySelectorAll( 'div[ data-role = "menuBackground" ]' ).forEach( function( background ) {
 				background.addEventListener( 'click', function() {
